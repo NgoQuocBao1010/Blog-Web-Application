4f7df8f36c72ebdf125dc30d303d2677ccdf4623
NgoQuocBao1010 <56999031+NgoQuocBao1010@users.noreply.github.com>
2021-11-16 16:25:37 +0700

    ✍ post description done, ✨ style post detail content

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -14,10 +14,22 @@ class Category(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+    description = models.CharField(default="This is a post description", max_length=255)
     coverImage = models.ImageField(default="posts/blog2.jpg", upload_to="posts/")
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     dateCreated = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    @property
+    def briefDescription(self):
+        words = self.description.split(" ")
+
+        if len(words) < 10:
+            return self.description
+
+        newWords = " ".join([word for index, word in enumerate(words) if index < 9])
+
+        return f"{newWords} ..."
 
     class Meta:
         ordering = ["-dateCreated"]
